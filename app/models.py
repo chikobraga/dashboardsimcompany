2346b253f6aca7247be0412c86d5e6864094da6c
chikobraga <chikob@gmail.com>
2020-07-11 16:52:13 -0300

    upload model

--- app/models.py
+++ app/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class AccountHistory(models.Model):
+    id = models.IntergerField(primary_key=True)
+    Timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
+    Category = models.CharField(max_length=50)
+    Money = models.DecimalField(max_digits=10, decimal_places=2)
+    Descriptions = models.CharField(max_length=50)
