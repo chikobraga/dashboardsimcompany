f2e8a606749d282ef3003c17d797f22078777aa3
chikobraga <chikob@gmail.com>
2020-07-12 09:27:02 -0300

    upload model1

--- app/models.py
+++ app/models.py
@@ -3,8 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 class AccountHistory(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.BigIntegerField(primary_key=True, editable=True)
     Timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
     Category = models.CharField(max_length=50)
     Money = models.DecimalField(max_digits=10, decimal_places=2)
     Descriptions = models.CharField(max_length=50)
+
+class Details(models.Model):
+    Accountid = models.ForeignKey(id, related_name='id_account', null=True, blank=True, on_delete=models.CASCADE)
+    Category = models.CharField(max_length=50)
+    Amount = models.CharField(max_length=50)
+    Quality = models.CharField(max_length=50)
+    Price = models.CharField(max_length=50)
+    Buyer = models.CharField(max_length=50)
+    Profit = models.CharField(max_length=50)
